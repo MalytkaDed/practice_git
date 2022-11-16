658da211aa3cbc10794a26f42d32242a7ca00639
MalytkaDed <pasha.lebedev.2004@gmail.com>
2022-11-16 21:45:15 +0300

    commit #5 (изменение программы)

--- max_numer.py
+++ max_numer.py
@@ -1,5 +1,13 @@
 import os
-a = input()
+
+a = ""
+kol = int(input("колличество элементов в массиве: "))
+print("массив: ")
+mas = list(map(int, input().split()))
+for i in range(kol - 1):
+    a += str(mas[i]) + " "
+a += str(mas[-1])
+
 max_a = -10**10
 k = 0
 for i in range(0, len(a)):
@@ -7,5 +15,5 @@ for i in range(0, len(a)):
         max_a = max(max_a, int(a[k:i]))
         k = i+1
 max_a = max(max_a, int(a[k: len(a)]))
-print(max_a)
+print("максимальное число: ",max_a)
 os.system("pause")
